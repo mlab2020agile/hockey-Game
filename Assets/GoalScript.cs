390dcaccf7d17e87f2e302a1cfa2c80239548a25
kusama0 <csma19805@g.nihon-u.ac.jp>
2020-05-22 15:04:17 +0900

    得失点機能を追加しました。

--- Assets/GoalScript.cs
+++ Assets/GoalScript.cs
@@ -1,25 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GoalScript : MonoBehaviour
 {
-    //OnCollisionEnter()
-    private void OnCollisionEnter(Collision collision)
-    {
-        //色を黒に変化させる
-        GetComponent<Renderer>().material.color = Color.black;
-    }
+     public int color =0,count;
+    public Text countText;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
-    }
+        count = 0;
+        countText.text = "得点"+count.ToString();
 
+    }
+    //OnCollisionEnter()
+    public void OnCollisionEnter(Collision collision)
+    {
+        //色を黒に変化させる
+        if (color == 0)
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+            color = 1;
+
+        } else if (color == 1) { GetComponent<Renderer>().material.color = Color.red; color = 0; }
+        count = count + 1;
+        countText.text = "得点" + count.ToString();
+    }
     // Update is called once per frame
     void Update()
     {
+        
         
     }
 }
---
+++ Assets/LostPoint.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LostPoint : MonoBehaviour
+{
+    GameObject back;
+    int  count,score;
+    GoalScript script;
+    
+    public Text countText;
+    // Start is called before the first frame update
+    void Start()
+    {   back = GameObject.Find("back");
+        script = back.GetComponent<GoalScript>();
+        int getpoint = script.count;
+       // Debug.Log(getpoint);
+        count = 0;
+        score = getpoint - count;
+        countText.text = "失点" + count.ToString()+"Score" + score.ToString();
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+       
+        count = count + 1;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    { int getpoint = script.count;
+        score = getpoint - count;
+        countText.text = "失点" + count.ToString()+"Score" + score.ToString();
+    }
+
+}
