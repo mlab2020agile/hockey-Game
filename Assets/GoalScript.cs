da2bee504f411289a05e02aa115cbff0acd5b9c7
mlab2020agile <mlab2020agile@gmail.com>
2020-05-27 13:42:58 +0900

    一時保存です

--- Assets/GoalScript.cs
+++ Assets/GoalScript.cs
@@ -1,38 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
 
 public class GoalScript : MonoBehaviour
 {
-     public int color =0,count;
-    public Text countText;
-
-    // Start is called before the first frame update
-    public void Start()
-    {
-        count = 0;
-        countText.text = "得点"+count.ToString();
-
-    }
     //OnCollisionEnter()
-    public void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         //色を黒に変化させる
-        if (color == 0)
-        {
-            GetComponent<Renderer>().material.color = Color.black;
-            color = 1;
-
-        } else if (color == 1) { GetComponent<Renderer>().material.color = Color.red; color = 0; }
-        count = count + 1;
-        countText.text = "得点" + count.ToString();
+        GetComponent<Renderer>().material.color = Color.black;
     }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
         
     }
 }
